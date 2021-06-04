f390c406b095dec002b5811f5e930575cc9eab51
TheSAA06 <60203764+TheSAA06@users.noreply.github.com>
2021-06-04 09:08:15 +0900

    Add files via upload

---
+++ playerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerController : MonoBehaviour
+{
+    int time = 20;
+    int hp = 100;
+    public Text timerText;
+    public Text hpText;
+    
+    float vert;
+    float horiz;
+    Transform cube_tr;
+    Rigidbody cube_rb;
+    void Timer()
+    {
+        time = time - 1;
+        timerText.text = "TIMER: "+time;
+    }
+    void Start()
+    {
+        InvokeRepeating("Timer",1f,1f);
+        cube_tr = GetComponent<Transform>();
+        cube_rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vert = Input.GetAxis("Vertical") * 4f;
+        horiz = Input.GetAxis("Horizontal") * 0.4f;
+        cube_rb.AddForce(cube_tr.forward * vert);
+        cube_tr.Rotate(0,horiz,0);
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "enemy")
+        {
+            hp = hp - 30;
+            hpText.text = "HP: "+hp;
+        }
+    }
+}
